632548da5c2553be568c91e79d479755a1605db6
Ulf Ivraeus <ulf.ivraeus@combitech.com>
2024-01-22 13:39:24 +0100

    Update kubectl install

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -12,7 +12,7 @@ set -uxo pipefail
 sudo apt update
 sudo apt install -y ca-certificates curl
 sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubectl
