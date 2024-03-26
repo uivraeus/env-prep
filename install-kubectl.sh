10a0bdf80646cb0581bbde63d8f0c93aaa12a974
Ulf Ivraeus <ulf.ivraeus@combitech.com>
2024-03-26 12:47:32 +0100

    Revise kubectl install procedure

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 
-# This scripts assumes Ubuntu-22.04 (with systemd)
-
 echo "***************************************************"
 echo "Installing kubectl"
 echo "***************************************************"
 
 set -uxo pipefail
 
-# Install kubectl [https://helm.sh/docs/intro/install/#from-script]
-sudo apt update
-sudo apt install -y ca-certificates curl
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
-sudo apt install -y kubectl
-
-
-
-
-
-
-
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+curl -s -L -o /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x /tmp/kubectl
+mkdir -p ~/.local/bin
+mv /tmp/kubectl ~/.local/bin/kubectl
